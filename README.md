e69ac995472acfa57df0b7f780f844719d555543
marjorie <marjorie.ngoupende@gmail.com>
2021-11-22 13:01:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Deploy NFT with Truffle, IPFS OpenSea & Polygon (Mumbai)
+### Visit https://testnets.opensea.io/assets/mumbai/0xf0ae167c87bd404d00893bf2da2b87d7483b00f1/1/ for see my NFT.
+### Prerequisite programming in node.js would be beneficial.
+
+# Getting started 
+Clone this repo. 
+Run `npm install` in terminal.
+* Place Matic RPC node key and Wallet Mnemonic in new .env file
+* Upload some nft metadata to an api. I used IPFS.
+* Place link to NFT Collection.json in contractURI method, under GameItem.sol. 
+* Deploy the contracts with `truffle develop` then `migrate --network mumbai` in the terminal, then copy the contract address.
+* Add your account address, contract address and token json link to the mint script.
+* Enter `node scripts/mint.js` into the console to mint.
+* For security reasons my files with my private keys have been deleted as well as the hashes of my images and my json files.
+
+
+This is inspired by opensea's tutorial: https://docs.opensea.io/docs/getting-started and https://docs.openzeppelin.com/contracts/4.x/erc721 .
+
+## A simple way to create NFT smart contracts and deploy on OpenSea to make our digital asset visible.Good work!
---
+++ contracts/GameItem.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract GameItem is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // set contract name and ticker. 
+    constructor() ERC721("Sweet Moon Nft", "SMN") {}
+
+    //get the current supply of tokens
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
+    }
+
+    // for opensea collection 
+    function contractURI() public pure returns (string memory) {
+        return "https://ipfs.io/ipfs/QmRopoE9cGHf6jyKjoH8oGiT6MCKmeADmA58wY1xiGhiuH";
+    }
+
+    
+    function mintItem(address player, string memory tokenURI)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+
+}
